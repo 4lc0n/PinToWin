795f5d5d89c67086d7913738f88261328521258e
MRPMerces <57101168+MRPMerces@users.noreply.github.com>
2021-11-24 10:07:22 +0100

    Added the unity simulation

---
+++ PinballSim-main/Pinball/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PaddleController : MonoBehaviour
+{
+    public float targetPosition = 0F;
+    public string inputButtonName = "LeftPaddle";
+
+    private new HingeJoint hingeJoint;
+    JointSpring JointSpring;
+
+    float triggertime = 0f;
+    float waitTime = 0f;
+    bool wait;
+
+    // Start is called before the first frame update
+    void Start() {
+        hingeJoint = GetComponent<HingeJoint>();
+
+        JointSpring = new JointSpring();
+
+        hingeJoint.useSpring = true;
+
+        JointLimits limits = hingeJoint.limits;
+        limits.min = 0;
+        limits.bounciness = 0;
+        limits.bounceMinVelocity = 0;
+        limits.max = targetPosition;
+        hingeJoint.useLimits = true;
+        hingeJoint.limits = limits;
+
+        JointSpring.spring = 1000000000;
+        JointSpring.damper = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if ((wait && waitTime <= 0) || Input.GetKey(inputButtonName)) {
+            triggerFlipper();
+            wait = false;
+        }
+
+        else {
+            waitTime -= Time.deltaTime;
+        }
+
+        if (triggertime > 0) {
+            triggertime -= Time.deltaTime;
+        }
+
+        else {
+            JointSpring.targetPosition = 0;
+            hingeJoint.spring = JointSpring;
+        }
+    }
+
+    public void activate(float activationDelay = 0) {
+        if (activationDelay == 0) {
+            triggerFlipper();
+            return;
+        }
+        waitTime = activationDelay;
+        wait = true;
+    }
+
+    Action<PaddleController> cbPaddleActivated;
+
+    public void RegisterPaddleActivated(Action<PaddleController> callbackfunc) {
+        cbPaddleActivated += callbackfunc;
+    }
+
+    void triggerFlipper() {
+        //Debug.Log("activate");
+        triggertime = 0.1f;
+
+        JointSpring.targetPosition = targetPosition;
+        hingeJoint.spring = JointSpring;
+        
+        cbPaddleActivated?.Invoke(this);
+    }
+}
---
+++ PinballSim-main/Pinball/Assets/Scripts/PullSpring.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullSpring : MonoBehaviour {
+
+    public string inputButtonName = "Pull";
+
+    public float distance = 50;
+    public float speed = 1;
+    public float power = 2000;
+
+    public GameObject ball;
+
+    private bool ready = false;
+    private bool fire = false;
+    private float moveCount = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(inputButtonName)) {
+            //As the button is held down, slowly move the piece
+            if (moveCount < distance) {
+                transform.Translate(0, 0, -speed * Time.deltaTime);
+                moveCount += speed * Time.deltaTime;
+                fire = true;
+            }
+        }
+
+        else if (moveCount > 0) {
+            //Shoot the ball
+            if (fire && ready) {
+                ball.transform.TransformDirection(Vector3.forward * 10);
+                ball.GetComponent<Rigidbody>().AddForce(0, 0, moveCount * power);
+                fire = false;
+                ready = false;
+            }
+            //Once we have reached the starting position fire off!
+            transform.Translate(0, 0, 20 * Time.deltaTime);
+            moveCount -= 20 * Time.deltaTime;
+        }
+
+        //Just ensure we don't go past the end
+        if (moveCount <= 0) {
+            fire = false;
+            moveCount = 0;
+        }
+    }
+
+    void OnCollisionEnter(Collision _other) {
+        if (_other.gameObject.tag == "Ball") {
+            ready = true;
+        }
+    }
+}
+
+
+
+// FIXME:: spamming space fucks everything.
+
